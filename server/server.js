fc0e2a76cf99db079961df9bec4b8b0436010df9
Andreas Schmelz <andreasschmelz@gmx.net>
2016-05-25 19:30:52 +0200

    use underscore from cdn

--- server/server.js
+++ server/server.js
@@ -30,18 +30,6 @@ var WebSocketServer = require('ws').Server;
 var __ = require('underscore');
 
 var PORT = process.env.PORT || 8080;
-/*
-var express = require("express");
-var http = require("http");
-var app = express();
-   app.use(express.static(__dirname + "/"));
-
-var server2 = http.createServer(app);
-server2.listen(PORT);*/
-    /*
-console.log("http server listening on %d", PORT);
-
-*/
 console.log('PORT ' + PORT);
 var Server = {
   wss: new WebSocketServer({port: PORT}),
